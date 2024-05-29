93360a96fb8f6f93bf2c806c1d3ce0d1fe0f2675
Akbar Setiyawan <akbar.23335@mhs.unesa.ac.id>
2024-05-29 17:10:45 +0700

    update

--- scripts.js
+++ scripts.js
@@ -3,7 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
         { title: 'DSA Project', description: 'Simple program about "Network Traffic Monitoring and Simulation of Tree Topology Network" using C++ to learn about Data Structure and Algorithm', url: 'https://github.com/arezyhs/cplusplus-network-traffic-monitoring-and-tree-topology-network' },
         { title: 'C++ Fundamental', description: 'This project is a simple text-based RPG game written in C++. Players navigate through a fantasy world, encountering enemies and making choices to progress in the game.', url: 'https://github.com/arezyhs/cplusplus-final-project-rpg-game' },
         { title: 'Project 3', description: 'Description of project 3', url: 'https://github.com/arezyhs/project3' },
-        // Add more projects here
+        { title: 'Project 3', description: 'Description of project 3', url: 'https://github.com/arezyhs/project3' },
+        { title: 'Project 3', description: 'Description of project 3', url: 'https://github.com/arezyhs/project3' },
+        { title: 'Project 3', description: 'Description of project 3', url: 'https://github.com/arezyhs/project3' },
+
+        // proyek baru tambah ke sini
     ];
 
     const projectList = document.querySelector('.project-list');
